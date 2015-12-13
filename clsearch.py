88804376c0f00db0b809f30da187d715818771ea
purple-goblin <purple-goblin@foo.com>
2015-12-13 11:11:18 -0500

    fixed job links

--- clsearch.py
+++ clsearch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import webbrowser, os, sys, urllib2
+import webbrowser, os, sys, requests
 from bs4 import BeautifulSoup
 
 # Replace the value of query with the URL string as described in the 
@@ -28,31 +28,38 @@ with open('subdomains.txt','r') as f:
 # query each subdomain and build list of jobs
 for sub in subdomains:
 	sub = sub.strip()
-	print 'Searching ' + sub + '.craigslist.org ...'
-	url = 'http://' + sub + '.craigslist.org/' + query
-	headers = {'User-agent': 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0'}
-	req = urllib2.Request(url, None, headers)
-	reply = urllib2.urlopen(req).read()
+	sys.stdout.write('Searching ' + sub + '.craigslist.org ...')
 
-	soup = BeautifulSoup(reply)
+	# build the search url, send request, and parse results
+	url     = 'http://' + sub + '.craigslist.org/' + query
+	headers = {'User-agent': 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0'}
+	req     = requests.get(url,headers=headers)
+	soup    = BeautifulSoup(req.text)
+
 	if soup('p',class_="row"):
 		para = soup('p',class_="row")
 	else:
 	# if there are no matches at all, break out of the current loop
+		sys.stdout.write("  no matches found\n")
 		continue
 
 	# if the previous sibling of the first item is h4, this is the NEARBY
 	# section (which we don't want), so break out of the current loop
 	if para[0].previous_sibling.previous_sibling:
 		if para[0].previous_sibling.previous_sibling.name == "h4":
+			sys.stdout.write("  no matches found\n")
 			continue
-
+	
+	# otherwise, we've found matches
+	sys.stdout.write("  found matches!\n")
 	matches = []
 	a = '<a href="' + url + '">' + sub + '</a>'
 	city = '<br><h3>Matches found in ' + a + ':</h3>'
 	matches.append(city)
 
 	for p in para:
+		job = p.a['href']
+		p('a')[1]['href'] = 'https://' + sub + '.craigslist.org' + job
 		matches.append(p)
 
 	with open('out.htm','a') as out:
